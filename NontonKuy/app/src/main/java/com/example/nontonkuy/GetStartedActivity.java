570d6148ef33968d0c60372284e8283eadd32093
RickyBR <berlandoricky@gmail.com>
2021-02-02 02:08:28 +0700

    add first sign up page and add intent

--- NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStartedActivity extends AppCompatActivity {
+    Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        btn_signup = findViewById(R.id.btn_signup);
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goSignup = new Intent(GetStartedActivity.this , FirstSignUpActivity.class);
+                startActivity(goSignup);
+            }
+        });
 
         
     }
