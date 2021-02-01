e24053bb701b8446d66d3bd3fd71e95486261c92
RickyBR <berlandoricky@gmail.com>
2021-02-01 22:59:35 +0700

    fix layout and put intent with delayed in SplashSceenActivity

--- NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
@@ -2,7 +2,9 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class GetStartedActivity extends AppCompatActivity {
 
@@ -10,6 +12,7 @@ public class GetStartedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
         
     }
 }
--- NontonKuy/app/src/main/java/com/example/nontonkuy/SplashSceenActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/SplashSceenActivity.java
@@ -2,7 +2,9 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashSceenActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class SplashSceenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_sceen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goGetStarted = new Intent(SplashSceenActivity.this, GetStartedActivity.class);
+                startActivity(goGetStarted);
+            }
+        },2000);
 
     }
 }
