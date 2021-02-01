c1ba74952cb95fb0630d227ce463c92234df1e50
RickyBR <berlandoricky@gmail.com>
2021-02-01 22:27:10 +0700

    add to github

---
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
@@ -0,0 +1,15 @@
+package com.example.nontonkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+        
+    }
+}
---
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/SplashSceenActivity.java
@@ -0,0 +1,15 @@
+package com.example.nontonkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashSceenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sceen);
+
+    }
+}
