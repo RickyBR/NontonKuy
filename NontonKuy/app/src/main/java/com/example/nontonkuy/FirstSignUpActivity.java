072ea72fa9362ed8aa9874180443aa97b1cd4a09
RickyBR <berlandoricky@gmail.com>
2021-02-04 00:21:39 +0700

    add second signup page and signin page

--- NontonKuy/app/src/main/java/com/example/nontonkuy/FirstSignUpActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/FirstSignUpActivity.java
@@ -2,13 +2,26 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstSignUpActivity extends AppCompatActivity {
 
+    Button btn_continue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_sign_up);
+        btn_continue = findViewById(R.id.btn_continue);
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gosecondpage = new Intent(FirstSignUpActivity.this , SecondSignUpAcitivity.class);
+                startActivity(gosecondpage);
+            }
+        });
     }
 }
--- NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/GetStartedActivity.java
@@ -9,13 +9,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class GetStartedActivity extends AppCompatActivity {
-    Button btn_signup;
+    Button btn_signup,btn_signin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
         btn_signup = findViewById(R.id.btn_signup);
+        btn_signin = findViewById(R.id.btn_Signin);
+
+        btn_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goSignin = new Intent(GetStartedActivity.this , SignInActivity.class);
+                startActivity(goSignin);
+            }
+        });
 
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
