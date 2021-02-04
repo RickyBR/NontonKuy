de42169d76e6d2d2b905372760aad0f0e752739d
RickyBR <berlandoricky@gmail.com>
2021-02-05 02:07:11 +0700

    Add scroll view in home page

--- NontonKuy/app/src/main/java/com/example/nontonkuy/SecondSignUpAcitivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/SecondSignUpAcitivity.java
@@ -2,13 +2,27 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondSignUpAcitivity extends AppCompatActivity {
+
+    Button btn_gohome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_sign_up_acitivity);
+        btn_gohome = findViewById(R.id.btn_gohome);
+
+        btn_gohome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gohome = new Intent(SecondSignUpAcitivity.this, HomeActivity.class);
+                startActivity(gohome);
+            }
+        });
     }
 }
