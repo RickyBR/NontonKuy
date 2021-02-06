a766f5288c20a13168996ceb9379c9cf46c5f784
RickyBR <berlandoricky@gmail.com>
2021-02-06 19:40:10 +0700

    make movie page and fix home page layout

--- NontonKuy/app/src/main/java/com/example/nontonkuy/HomeActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/HomeActivity.java
@@ -2,13 +2,27 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageView movies_navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        movies_navbar = findViewById(R.id.movies_navbar);
+
+        movies_navbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goMoviesPage = new Intent(HomeActivity.this, MoviesActivity.class);
+                startActivity(goMoviesPage);
+            }
+        });
     }
 }
