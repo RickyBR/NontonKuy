eb58bd83f824ac14a6a664ba77b706274b5ff92d
RickyBR <berlandoricky@gmail.com>
2021-02-07 03:15:10 +0700

    make movie detail and half buy ticket detail layout

---
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/Movie_Detail_Activity.java
@@ -0,0 +1,14 @@
+package com.example.nontonkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Movie_Detail_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie__detail_);
+    }
+}
--- NontonKuy/app/src/main/java/com/example/nontonkuy/MoviesActivity.java
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/MoviesActivity.java
@@ -2,13 +2,26 @@ package com.example.nontonkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MoviesActivity extends AppCompatActivity {
+    Button btn_buy_joker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        btn_buy_joker = findViewById(R.id.btn_buy_joker);
+
+        btn_buy_joker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBuyJoker = new Intent(MoviesActivity.this, Movie_Detail_Activity.class);
+                startActivity(goBuyJoker);
+            }
+        });
     }
 }
