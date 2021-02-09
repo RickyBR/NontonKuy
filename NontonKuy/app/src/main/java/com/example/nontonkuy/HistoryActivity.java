4cd47b7614fed64140715c10c7989467895cbc64
RickyBR <berlandoricky@gmail.com>
2021-02-09 20:34:23 +0700

    Finish History Bookings

---
+++ NontonKuy/app/src/main/java/com/example/nontonkuy/HistoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.nontonkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+}
